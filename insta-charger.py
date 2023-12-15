51266f3fb3e0f5b71188a1f5f164d9c151d08966
Laercio Monteiro <laerciophb13@hotmail.com>
2023-12-15 15:58:20 -0300

    Otimizações no script para uma melhor legibilidade e organização

--- insta-charger.py
+++ insta-charger.py
@@ -3,62 +3,50 @@ from tqdm import tqdm
 from datetime import datetime
 from instaloader.exceptions import ProfileNotExistsException
 
-L = instaloader.Instaloader()
+def ler_usernames(arquivo):
+    with open(arquivo, 'r') as f:
+        return [username.strip() for username in f.readlines()]
 
-# Ler nomes de usuário do arquivo
-with open('agenda.txt', 'r') as f:
-    usernames = f.readlines()
-    usernames = [username.strip() for username in usernames]
+def obter_posts_recentes(usernames, mes_atual):
+    L = instaloader.Instaloader()
+    resultados = []
 
-results = []
-
-# Obter mês atual
-now = datetime.now()
-current_month = now.month
-
-for username in tqdm(usernames, desc="Coletando dados das contas", unit="contas"):
-    try:
-        profile = instaloader.Profile.from_username(L.context, username)
-
-        # Iterar pelos posts do perfil em ordem cronológica
-        for post in profile.get_posts():
-
-            # Verificar se o post é do mês atual
-            if post.date_local.month != current_month:
+    for username in tqdm(usernames, desc="Coletando dados das contas", unit="contas"):
+        try:
+            perfil = instaloader.Profile.from_username(L.context, username)
+            for post in perfil.get_posts():
+                if post.date_local.month != mes_atual:
+                    break
+                resultados.append({
+                    'username': username,
+                    'post_url': f"https://www.instagram.com/p/{post.shortcode}/",
+                    'timestamp': post.date_local.timestamp(),
+                    'likes': post.likes,
+                    'comments': post.comments,
+                    'caption': post.caption,
+                    'data': post.date_local.strftime("%d/%m/%Y")
+                })
                 break
+        except ProfileNotExistsException:
+            print(f"O usuário {username} não existe no Instagram. Ignorando...")
+        except Exception as e:
+            print(f"Não foi possível obter dados do usuário {username}: {str(e)}")
 
-            # Extrair informações relevantes do post mais recente
-            post_info = {
-                'username': username,
-                'post_url': f"https://www.instagram.com/p/{post.shortcode}/",
-                'timestamp': post.date_local.timestamp(),
-                'likes': post.likes,
-                'comments': post.comments,
-                'caption': post.caption,
-                'data': post.date_local.strftime("%d/%m/%Y")
-            }
+    return sorted(resultados, key=lambda x: x['likes'], reverse=True)
 
-            results.append(post_info)
-            break  # Sair do loop após o primeiro post
+def salvar_resultados(resultados, arquivo):
+    with open(arquivo, 'w') as f:
+        f.write("==> AGENDA DE EVENTOS <==\n\n")
+        for post_info in resultados:
+            f.write(f"Usuário: {post_info['username']}\nLink de acesso: {post_info['post_url']}\n")
+            f.write(f"Data do post: {post_info['data']}\n")
+            f.write(f"Curtidas: {post_info['likes']}, Comentários: {post_info['comments']}\n")
+            f.write(f"Legenda: {post_info['caption']}\n\n")
 
-    except ProfileNotExistsException:
-        print(f"O usuário {username} não existe no Instagram. Ignorando...")
-    except Exception as e:
-        print(f"Não foi possível obter dados do usuário {username}: {str(e)}")
-
-# Ordenar resultados por número de likes
-results = sorted(results, key=lambda x: x['likes'], reverse=True)
-
-# Exportar resultados para o arquivo resultados.txt
-with open('resultados.txt', 'w') as f:
-    f.write("==> AGENDA DE EVENTOS <==\n\n")
-    for post_info in results:
-        f.write(f"Usuário: {post_info['username']}\nLink de acesso: {post_info['post_url']}\n")
-        f.write(f"Data do post: {post_info['data']}\n")
-        f.write(f"Curtidas: {post_info['likes']}, Comentários: {post_info['comments']}\n")
-        f.write(f"Legenda: {post_info['caption']}\n\n")
-print("\n")
-# Mensagem de conclusão
-print("Processo finalizado!\n")
-print(f"Os resultados da coleta podem ser encontrados no arquivo 'resultados.txt'.")
-
+if __name__ == "__main__":
+    usernames = ler_usernames('agenda.txt')
+    mes_atual = datetime.now().month
+    resultados = obter_posts_recentes(usernames, mes_atual)
+    salvar_resultados(resultados, 'resultados.txt')
+    print("Processo finalizado!")
+    print("Os resultados da coleta podem ser encontrados no arquivo 'resultados.txt'.")
